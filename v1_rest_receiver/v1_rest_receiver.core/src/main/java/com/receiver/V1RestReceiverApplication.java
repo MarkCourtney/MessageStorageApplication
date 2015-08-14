e080fc48cb39768a6c8dd9b769ef9ac6005b49de
Mark Courtney <immarkc@gmail.com>
2015-08-14 12:20:02 +0100

    Setup project structure, ready to build Rest Receiver code for JSON

---
+++ v1_rest_receiver/v1_rest_receiver.core/src/main/java/com/receiver/V1RestReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1RestReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V1RestReceiverApplication.class, args);
+    }
+}
