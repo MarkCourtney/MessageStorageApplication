542fff84a8e5fdc64049b8cee72ccaf865af3ee0
Mark Courtney <immarkc@gmail.com>
2015-08-17 10:16:06 +0100

    Container factory created that sends a map to a destination of 'jms-receiver'

--- v1_rest_receiver/v1_rest_receiver.core/src/main/java/com/receiver/JsonController.java
+++ v1_rest_receiver/v1_rest_receiver.core/src/main/java/com/receiver/JsonController.java
@@ -1,7 +1,5 @@
 package com.receiver;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.jms.*;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import java.util.Map;
 
 /**
@@ -24,12 +20,27 @@ import java.util.Map;
 @RestController
 public class JsonController {
 
-    @RequestMapping(value = "/json", method = RequestMethod.POST)
-    public void getRequest(@RequestBody Map person) {
+    @Autowired
+    ConfigurableApplicationContext context;
 
-        System.out.println(person);
-        // Perform some altering if required
-        // person.put("id", UUID.randomUUID().toString());
-        // person.put("date", new Timestamp(System.currentTimeMillis()));
+    // Setup a producer to send a message with JMS
+    @Bean
+    JmsListenerContainerFactory<?> jsonListener(ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+
+    // Endpoint /json
+    // Json message will be sent here with POST request
+    // Send that message onward to a JMS consumer
+    @RequestMapping(value = "/json", method = RequestMethod.POST)
+    public void sendRequest(@RequestBody Map person) {
+        sendJmsMap("jms-receiver", person);
+    }
+
+    private void sendJmsMap(String destination, Map map) {
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        jmsTemplate.convertAndSend(destination, map);
     }
 }
